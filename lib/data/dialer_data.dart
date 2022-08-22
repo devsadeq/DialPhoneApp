00c023b838699d567f0b64137150de6511357ad6
devsadeq <dev.sadeq@gmail.com>
2022-08-22 03:44:54 +0300

    create demo data 👍

---
+++ lib/data/dialer_data.dart
@@ -0,0 +1,41 @@
+const List<Set<String>> dialNumbers = [
+  {'1', ''},
+  {'2', 'ABC'},
+  {'3', 'DEF'},
+  {'4', 'GHI'},
+  {'5', 'JKL'},
+  {'6', 'MNO'},
+  {'7', 'PQRS'},
+  {'8', 'TUV'},
+  {'9', 'WXYZ'},
+  {'*', ''},
+  {'0', '+'},
+  {'#', ''},
+];
+
+const List<String> randomsNames = [
+  "Sebulba Princess",
+  "PROXY Galen",
+  "Greedo Quinlan",
+  "Zam Luminara",
+  "Jerec Senator",
+  "Carnor Luminara",
+  "Han Lumiya",
+  "Luminara Quinlan",
+  "Revan Mission",
+  "Kyp Grand",
+  "Alistair Richie",
+  "Wade Elon",
+  "Zed Ledger",
+  "Henri Kyrin",
+  "Jayden Vikram",
+  "Henley Leo",
+  "Parker Juan",
+  "Zander Corbin",
+  "Kobe Paco",
+  "Enrique Ira",
+];
+
+List<String> contacts = [];
+
+var currentNumber = "";
